8bc3faf88376b2cf69425fc941e1a2a05ac79aa8
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-08-30 18:55:57 +0000

    Add code to free a linked list

--- 2. Manejo de memoria/ejemplo_listas.cpp
+++ 2. Manejo de memoria/ejemplo_listas.cpp
@@ -13,12 +13,57 @@ void insert(Node*& list, int x)
     list = p;
 }
 
+void display(Node* list)
+{
+    Node* current = list;
+    std::cout << "[";
+    while (current) {
+        std::cout << current->value << " ";
+        current = current->next;
+    }
+    std::cout << "]\n";
+}
+
+int length(Node* list)
+{
+    Node* current = list;
+    int counter = 0;
+    while (current) {
+        counter++;
+        current = current->next;
+    }
+    return counter;
+}
+
+void dispose(Node*& list)
+{
+    Node* current = list;
+    while (list) {
+        list = list->next;
+        delete current;
+        current = list;
+    }
+}
+
 int main()
 {
-    Node* list = nullptr;
-    insert(list, 7);
-    insert(list, 10);
-    insert(list, 4);
+    Node* start = nullptr;
+    display(start);
+    std::cout << "length(start) = " << length(start) << "\n";
+    insert(start, 7);
+    insert(start, 10);
+    insert(start, 4);
+    insert(start, 20);
+    display(start);
+    std::cout << "length(start) = " << length(start) << "\n";
+
+    Node* other = nullptr;
+    insert(other, 8);
+    insert(other, 9);
+    display(other);
+
+    dispose(start);
+    dispose(other);
 
     return 0;
 }
