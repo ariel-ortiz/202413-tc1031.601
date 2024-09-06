c84cfd3ce7db2fbbc2a9eafeba6b999e1477f04c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2024-09-06 18:57:44 +0000

    Add code for final version of LinkedList class

--- 3. LinkedList/linkedlist.h
+++ 3. LinkedList/linkedlist.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <sstream>
+#include <stdexcept>
 
 template<typename T>
 class LinkedList {
@@ -16,6 +17,29 @@ public:
         _sentinel = p;
     }
 
+    LinkedList(std::initializer_list<T> args) : LinkedList()
+    {
+        for (T arg : args) {
+            insert_back(arg);
+        }
+    }
+
+    LinkedList(const LinkedList& other) = delete;
+
+    LinkedList<T>& operator=(const LinkedList& other) = delete;
+
+    // Complexity: O(N)
+    ~LinkedList()
+    {
+        Node* p = _sentinel->next;
+        while (p != _sentinel) {
+            Node* q = p;
+            p = p->next;
+            delete q;
+        }
+        delete _sentinel;
+    }
+
     // Complexity: O(1)
     void insert_front(T value)
     {
@@ -26,6 +50,40 @@ public:
         _sentinel->next->prev = p;
         _sentinel->next = p;
         _size++;
+    }
+
+    // Complexity: O(1)
+    void insert_back(T value)
+    {
+        Node* p = new Node;
+        p->value = value;
+        p->next = _sentinel;
+        p->prev = _sentinel->prev;
+        _sentinel->prev->next = p;
+        _sentinel->prev = p;
+        _size++;
+    }
+
+    // Complexity: O(1)
+    bool is_empty() const
+    {
+        return _size == 0;
+    }
+
+    // Complexity: O(1)
+    T remove_front()
+    {
+        if (is_empty()) {
+            throw std::length_error(
+                "Can't remove from an empty list");
+        }
+        Node* p = _sentinel->next;
+        T result = p->value;
+        _sentinel->next = p->next;
+        p->next->prev = _sentinel;
+        delete p;
+        _size--;
+        return result;
     }
 
     // Complexity: O(1)
--- 3. LinkedList/main.cpp
+++ 3. LinkedList/main.cpp
@@ -5,9 +5,17 @@ int main()
 {
     LinkedList<std::string> a;
     std::cout << a.to_string() << "\n";
-    a.insert_front("lunes");
-    a.insert_front("martes");
-    a.insert_front("miércoles");
+    a.insert_back("lunes");
+    a.insert_back("martes");
+    a.insert_back("miércoles");
     std::cout << a.to_string() << "\n";
+
+    LinkedList<int> b {4, 8, 15, 16, 23, 42};
+    std::cout << b.remove_front() << "\n";
+    std::cout << b.remove_front() << "\n";
+    std::cout << b.remove_front() << "\n";
+    std::cout << b.to_string() << "\n";
+    std::cout << b.size() << "\n";
+
     return 0;
 }
